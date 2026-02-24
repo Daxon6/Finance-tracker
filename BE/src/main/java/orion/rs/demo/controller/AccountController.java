a25c827a037b5597a0fcbfd736c34e4e8b6936fa
Aleksa Antic <aleksa.antic@mera.com>
2026-02-24 11:49:03 +0100

    read all accounts commit

--- BE/src/main/java/orion/rs/demo/controller/AccountController.java
+++ BE/src/main/java/orion/rs/demo/controller/AccountController.java
@@ -1,26 +1,40 @@
 package orion.rs.demo.controller;
 
 import jakarta.validation.Valid;
+import org.springframework.http.MediaType;
+import orion.rs.demo.domain.Account;
 import orion.rs.demo.dto.AccountCreateDTO;
 import orion.rs.demo.dto.AccountDTO;
 import orion.rs.demo.service.AccountService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import orion.rs.demo.service.implementation.AccountServiceImpl;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/accounts")
 public class AccountController {
 
     private final AccountService accountService;
-
-    public AccountController(AccountService accountService) {
+    private final AccountServiceImpl accountServiceImpl;
+    public AccountController(AccountService accountService, AccountServiceImpl accountServiceImpl) {
         this.accountService = accountService;
+        this.accountServiceImpl = accountServiceImpl;
     }
 
     @PostMapping
     public ResponseEntity<AccountDTO> createAccount(@Valid @RequestBody AccountCreateDTO accountCreateDTO) {
         AccountDTO savedAccount = accountService.createAccount(accountCreateDTO);
         return new ResponseEntity<>(savedAccount, HttpStatus.CREATED);
+    }
+
+    /**
+     * Get all accounts from dataBase
+     * */
+    @GetMapping(value = "getAccounts",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Account>> getAllAcc(){
+        return ResponseEntity.status(HttpStatus.OK).body(accountServiceImpl.getAllAcc());
     }
 }
